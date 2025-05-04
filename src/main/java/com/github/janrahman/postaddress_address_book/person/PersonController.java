431367c86833b07163d5672de9b979b00a5a9a62
Jan Rahman <44271158+janrahman@users.noreply.github.com>
2025-05-05 00:55:43 +0200

    create PersonController

---
+++ src/main/java/com/github/janrahman/postaddress_address_book/person/PersonController.java
@@ -0,0 +1,53 @@
+package com.github.janrahman.postaddress_address_book.person;
+
+import com.github.janrahman.postaddress_address_book.openapi.api.PersonsApi;
+import com.github.janrahman.postaddress_address_book.openapi.model.AddPerson;
+import com.github.janrahman.postaddress_address_book.openapi.model.Address;
+import com.github.janrahman.postaddress_address_book.openapi.model.NewAddress;
+import com.github.janrahman.postaddress_address_book.openapi.model.Person;
+import com.github.janrahman.postaddress_address_book.openapi.model.PersonsIdAddressesPostRequest;
+import com.github.janrahman.postaddress_address_book.openapi.model.UpdatePersonInfo;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PersonController implements PersonsApi {
+
+  @Override
+  public ResponseEntity<List<Person>> _personsGet(
+      String street, String streetNumber, String postalCode, String city) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Address> _personsIdAddressesGet(Long id, NewAddress newAddress) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Address> _personsIdAddressesPost(
+      Long id, PersonsIdAddressesPostRequest personsIdAddressesPostRequest) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Void> _personsIdDelete(Long id) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Person> _personsIdGet(Long id) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Person> _personsIdPut(Long id, UpdatePersonInfo updatePersonInfo) {
+    return null;
+  }
+
+  @Override
+  public ResponseEntity<Person> _personsPost(AddPerson addPerson) {
+    return null;
+  }
+}
